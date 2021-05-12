32d7bfdf5436d10394bf7f77086a9fb35b0497e0
marician-net <luis.morales.free@gmail.com>
2021-05-12 14:24:29 -0400

    Added assertion to main_test.go

--- tracker/main_test.go
+++ tracker/main_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/zapproject/zap-miner/apiOracle"
 	"github.com/zapproject/zap-miner/config"
 	"github.com/zapproject/zap-miner/util"
@@ -30,5 +31,25 @@ func TestMain(m *testing.M) {
 	}
 	util.ParseLoggingConfig("")
 	apiOracle.EnsureValueOracle()
+	// assert for indexes
 	os.Exit(m.Run())
+}
+
+func TestMockedConfig(t *testing.T) {
+	// assert for configs
+	cfg := config.GetConfig()
+	assert.Equal(t, "0000000000000000000000000000000000000000", cfg.PublicAddress)
+	assert.Equal(t, "1111111111111111111111111111111111111111111111111111111111111111", cfg.PrivateKey)
+	assert.Equal(t, "0x724D1B69a7Ba352F11D73fDBdEB7fF869cB22E19", cfg.ContractAddress)
+	assert.Equal(t, "..", cfg.IndexFolder)
+	assert.Equal(t, 1.0, cfg.DisputeThreshold)
+	assert.Equal(t, config.Duration(config.Duration{Duration: 50000000000}), cfg.DisputeTimeDelta)
+
+	// assert trackers individually
+	assert.Equal(t, "indexers", cfg.Trackers[0])
+	assert.Equal(t, "balance", cfg.Trackers[1])
+	assert.Equal(t, "currentVariables", cfg.Trackers[2])
+	assert.Equal(t, "disputeStatus", cfg.Trackers[3])
+	assert.Equal(t, "gas", cfg.Trackers[4])
+	assert.Equal(t, "disputeChecker", cfg.Trackers[5])
 }
