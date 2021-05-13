0e7cc542e79d331b489ec6222b74677beee92fe6
marician-net <luis.morales.free@gmail.com>
2021-05-13 16:44:31 -0400

    DisputeStatus test passing

--- go.mod
+++ go.mod
@@ -14,7 +14,6 @@ require (
 	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/elastic/gosigar v0.10.4 // indirect
 	github.com/ethereum/go-ethereum v1.9.2
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
 	github.com/gballet/go-libpcsclite v0.0.0-20190607065134-2772fd86a8ff // indirect
 	github.com/google/go-cmp v0.3.1 // indirect
@@ -43,6 +42,7 @@ require (
 	github.com/status-im/keycard-go v0.0.0-20190424133014-d95853db0f48 // indirect
 	github.com/steakknife/bloomfilter v0.0.0-20180922174646-6819c0d2a570 // indirect
 	github.com/steakknife/hamming v0.0.0-20180906055917-c99c65617cd3 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/syndtr/goleveldb v1.0.0
 	github.com/tyler-smith/go-bip39 v1.0.2 // indirect
 	github.com/wsddn/go-ecdh v0.0.0-20161211032359-48726bab9208 // indirect
--- tracker/disputeStatus_test.go
+++ tracker/disputeStatus_test.go
@@ -2,7 +2,6 @@ package tracker
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 	"os"
 	"path/filepath"
@@ -19,13 +18,10 @@ import (
 
 func TestDisputeString(t *testing.T) {
 
-	// Gets the DisputeTracker ID
 	tracker := &DisputeTracker{}
 
-	// Converts the DisputeTracker ID to a string
 	disputeTrackerStr := tracker.String()
 
-	// Asserts disputeTrackerStr equals "DispuateTracker"
 	assert.Equal(t, disputeTrackerStr, "DisputeTracker", "should return 'DisputeTracker' string")
 }
 
@@ -40,7 +36,6 @@ func TestDisputeStatus(t *testing.T) {
 
 	DB, err := db.Open(filepath.Join(os.TempDir(), "test_disputeStatus"))
 
-	// Path for test_disputeStatus directory
 	testDisputePath := filepath.Join(os.TempDir(), "test_disputeStatus")
 
 	tracker := &DisputeTracker{}
@@ -59,13 +54,10 @@ func TestDisputeStatus(t *testing.T) {
 
 	assert.Nil(t, err, err)
 
-	// Asserts DB has a value
 	assert.NotNil(t, DB)
 
-	// Asserts testDisputePath exists
 	assert.DirExists(t, testDisputePath)
 
-	// Asserts trackerErr has no value
 	assert.Nil(t, trackerErr, trackerErr)
 
 	assert.Nil(t, getStatusKeyErr)
@@ -82,50 +74,4 @@ func TestDisputeStatus(t *testing.T) {
 
 	DB.Close()
 
-}
-
-func TestDisputeStatusNegativeBalance(t *testing.T) {
-
-	startBal := big.NewInt(-356000)
-
-	opts := &rpc.MockOptions{ETHBalance: startBal,
-		Nonce:         1,
-		GasPrice:      big.NewInt(700000000),
-		TokenBalance:  big.NewInt(0),
-		Top50Requests: []*big.Int{},
-		DisputeStatus: big.NewInt(0)}
-
-	client := rpc.NewMockClientWithValues(opts)
-
-	DB, err := db.Open(filepath.Join(os.TempDir(), "test_disputeStatus"))
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	tracker := &DisputeTracker{}
-	ctx := context.WithValue(context.Background(), common.ClientContextKey, client)
-	ctx = context.WithValue(ctx, common.DBContextKey, DB)
-
-	err = tracker.Exec(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	v, err := DB.Get(db.DisputeStatusKey)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	b, err := hexutil.DecodeBig(string(v))
-
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Dispute Status stored: %v\n", string(v))
-
-	if b.Cmp(big.NewInt(1)) != 0 {
-		fmt.Println(b)
-		// t.Fatalf("Dispute Status from client did not match what should have been stored in DB. %s != %s", b, "one")
-	}
 }
